7cd216162854c6818a923224e49444b1fd4d0c0c
GeraldCNelson <nelson.gerald.c@gmail.com>
2020-10-20 09:08:36 -0600

    more cleanup of thiCalcs2.R

--- R/thiCalcs2.R
+++ R/thiCalcs2.R
@@ -84,20 +84,13 @@ readRast_thi_swine <- function(yearNumber) {
   r <- round(rast(fileNameIn), 2)
 }
 
-THIfun_cattle <- function(cellVector) {
-  rh <- cellVector[1:vlend]
-  tmax <- cellVector[v2start:nl]
-  if (is.nan(cellVector[1])) { return(rep(NA, vlend)) }
-  #  print(paste("length tmax: ", (length(tmax))))
+
+THIfun_cattle <- function(rh, tmax) {
   thi <- (1.8 * tmax + 32.0) - ((0.55 - 0.0055 * rh) * (1.8 * tmax - 26.8))
   thi[tmax < 20] <- 0
   thi <- round(thi, 2)
 }
-
-
-THIfun_sheep <- function(cellVector) {
-  rh <- cellVector[1:vlend]
-  tmax <- cellVector[v2start:nl]
+THIfun_sheep <- function(rh, tmax) {
   if (is.nan(cellVector[1])) { return(rep(NA, vlend)) }
   #  print(paste("length tmax: ", (length(tmax))))
   thi <- tmax - ((0.31 - (0.31 * (rh / 100))) * (tmax - 14.4)) 
@@ -106,31 +99,26 @@ THIfun_sheep <- function(cellVector) {
 }
 
 THIfun_goat <- function(rh, tmax) {
-#  if (is.nan(rh[1])) { return(rep(NA, vlend)) }
-  #  print(paste("length tmax: ", (length(tmax))))
-  thi <- (1.8 * tmax + 32.0) - ((0.55 - 0.0055 * rh) * (1.8 * tmax - 26.8)) 
+  thi <- (1.8 * tmax + 32.0) - ((0.55 - 0.0055 * rh) * (1.8 * tmax - 26.8)) # should this be the same as cattle?
   thi[tmax < 20] <- 0
   thi <- round(thi, 2)
 }
 
-THIfun_swine <- function(cellVector) {
-  rh <- cellVector[1:vlend]
-  tmax <- cellVector[v2start:nl]
-  if (is.nan(cellVector[1])) { return(rep(NA, vlend)) }
-  #  print(paste("length tmax: ", (length(tmax))))
+THIfun_swine <- function(rh, tmax) {
   thi <- tmax - (0.55 - (0.0055 * rh) * (tmax - 14.5))
   thi[tmax < 20] <- 0
   thi <- round(thi, 2)
 }
 
-THIfun_chicken <- function(tmax) {
+THIfun_chicken <- function(tmax, tmin) {
   thi <- 0.60 * tmax + 0.40 * tmin # using broiler formula. Note. no rh needed
-  thi[thi < 0 ] <- 0
+  thi[tmax < 20] <- 0
+  thi <- round(thi, 2)
 }
 
 #THI scenarios -----
-speciesChoice <- c("goat", "swine", "chicken", "sheep") # cattle
-speciesChoice <- "goat"
+speciesChoice <- c("cattle", "goat", "swine", "chicken", "sheep") # cattle
+# speciesChoice <- "goat"
 for (k in sspChoices) {
   for (l in startyearChoices) {
     for (i in modelChoices) {
@@ -142,24 +130,18 @@ for (k in sspChoices) {
       rh <- rast(fileName_rh)
       comb <- sds(rh, tmax)
       
-      combined <- c(rh, tmax)
-      nl <- nlyr(combined)
-      vlend <- nl/2
-      v2start <- 1 + vlend
-      gc()
-      # startDate <- paste0(yearNumber, "-01-01"); endDate <- paste0(yearNumber, "-12-31")
-      # indicesYear <- seq(as.Date(startDate), as.Date(endDate), 1)
-      # indicesCharYear <- paste0("X", as.character(indicesYear))
-      # 
-      # print(system.time(tmax_yr <- subset(tmax, indicesCharYear)))
-      # print(system.time(rh_yr <- subset(rh, indicesCharYear)))
-      for (m in speciesChoice) {
+      # combined <- c(rh, tmax)
+      # nl <- nlyr(combined)
+      # vlend <- nl/2
+      # v2start <- 1 + vlend
+     gc()
+     for (m in speciesChoice) {
         
         fileName_out <- paste0("data/cmip6/THI/thi.", m, "_",  i, "_", k, "_", yearSpan, ".tif")
         print(paste0("fileName out: ", fileName_out))
         funName <- paste0("THIfun_", m)
         if (m %in% "chicken") {
-          comb_chicken <- tmax_yr
+          comb_chicken <- sds(rh, tmin)
           print(system.time(r_out <- lapp(comb_chicken, THIfun_chicken, filename = fileName_out, overwrite =TRUE, wopt = woptList)))
         } else {
           print(system.time(r_out <- lapp(comb, fun = funName, filename = fileName_out, overwrite =TRUE, wopt = woptList)))
@@ -176,27 +158,26 @@ for (k in sspChoices) {
 k <- "historical"
 for (i in modelChoices) {
   for (l in startyearChoices_historical) {
-    yearSpan <- paste0(l, "_", l + yearRange + 1) # the +1 here is to get at the original file names
-    modelName.lower <- tolower(i)
-    modelName.lower <- tolower(i)
+    comb <- sds(rh, tmax)
     yearSpan <- paste0(l, "_", l + yearRange)
+    modelName.lower <- tolower(i)
     fileName_tasmax <- paste0(locOfFiles,  modelName.lower, "_", k, "_tasmax_global_daily_", yearSpan, ".tif")
     tmax <- rast(fileName_tasmax)
     fileName_rh <- paste0(locOfFiles, modelName.lower, "_", k, "_hurs_global_daily_", yearSpan, ".tif")
     rh <- rast(fileName_rh)
-    
-    for (yearNumber in l:(l + yearRange)) {
-      gc()
-      startDate <- paste0(yearNumber, "-01-01"); endDate <- paste0(yearNumber, "-12-31")
-      indicesYear <- seq(as.Date(startDate), as.Date(endDate), 1)
-      indicesCharYear <- paste0("X", as.character(indicesYear))
-      
-      print(system.time(tmax_yr <- subset(tmax, indicesCharYear)))
-      print(system.time(rh_yr <- subset(rh, indicesCharYear)))
-      comb <- sds(rh_yr, tmax_yr)
-      fileName_out <- paste0("data/cmip6/THI/thi.cattle_",  i, "_", k, "_", yearNumber, ".tif")
-      
-      print(system.time(r_out <- lapp(comb, THIfun_cattle, filename = fileName_out, overwrite =TRUE, wopt = woptList)))
+    for (m in speciesChoice) {
+      fileName_out <- paste0("data/cmip6/THI/thi.", m, "_",  i, "_", k, "_", yearSpan, ".tif")
+      print(paste0("fileName out: ", fileName_out))
+      funName <- paste0("THIfun_", m)
+      if (m %in% "chicken") {
+        comb_chicken <- sds(rh, tmin)
+        print(system.time(r_out <- lapp(comb_chicken, THIfun_chicken, filename = fileName_out, overwrite =TRUE, wopt = woptList)))
+      } else {
+        print(system.time(r_out <- lapp(comb, fun = funName, filename = fileName_out, overwrite =TRUE, wopt = woptList)))
+        print(paste0("app done"))
+        
+        print(system.time(writeRaster(r_out, filename = fileName_out, overwrite=TRUE, wopt = woptList))); flush.console()
+      }
     }
   }
 }
