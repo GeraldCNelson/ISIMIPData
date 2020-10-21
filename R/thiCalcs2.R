e7eedc88d1ed127ee277b4dc0e548d13982aec1b
GeraldCNelson <nelson.gerald.c@gmail.com>
2020-10-21 10:50:42 -0600

    more fixes to thiCalcs.R

--- R/thiCalcs2.R
+++ R/thiCalcs2.R
@@ -17,7 +17,7 @@ get_os <- function() {
   tolower(os)
 }
 if (get_os() %in% "osx") {
-  terraOptions(memfrac = .9,  progress = 10, tempdir =  "data/ISIMIP", verbose = TRUE) # need to use a relative path
+  terraOptions(memfrac = .9, progress = 10, tempdir =  "data/ISIMIP", verbose = TRUE) # need to use a relative path, memfrac = .9,  
 }else{
   terraOptions(memfrac = .6,  progress = 10, tempdir =  "data/ISIMIP", verbose = TRUE) # need to use a relative path
 }
@@ -25,22 +25,18 @@ if (get_os() %in% "osx") {
 
 locOfFiles <- locOfCMIP6tifFiles
 locOfFiles <- "data/bigFiles/"
+speciesChoice <- c("cattle", "goat", "swine", "chicken", "sheep") 
 
 sspChoices <- c("ssp126", "ssp585") 
 sspChoices <- c("ssp585") 
 modelChoices <- c( "GFDL-ESM4", "MPI-ESM1-2-HR", "MRI-ESM2-0", "UKESM1-0-LL", "IPSL-CM6A-LR") #, "MPI-ESM1-2-HR", "MRI-ESM2-0", "IPSL-CM6A-LR") # "GFDL-ESM4", "MPI-ESM1-2-HR", "MRI-ESM2-0", "UKESM1-0-LL", "IPSL-CM5A-LR"
-# modelChoices <- c( "MPI-ESM1-2-HR", "MRI-ESM2-0", "UKESM1-0-LL", "IPSL-CM6A-LR") #, "MPI-ESM1-2-HR", "MRI-ESM2-0", "IPSL-CM6A-LR") # "GFDL-ESM4", "MPI-ESM1-2-HR", "MRI-ESM2-0", "UKESM1-0-LL", "IPSL-CM5A-LR"
+#modelChoices <- c( "MPI-ESM1-2-HR", "MRI-ESM2-0", "UKESM1-0-LL", "IPSL-CM6A-LR") #, "MPI-ESM1-2-HR", "MRI-ESM2-0", "IPSL-CM6A-LR") # "GFDL-ESM4", "MPI-ESM1-2-HR", "MRI-ESM2-0", "UKESM1-0-LL", "IPSL-CM5A-LR"
 startyearChoices <-  c(2041, 2081) #2011, 2041, 2051, 2081) # c(2091) # c(2006) #, 2041, 2051, 2081)
 #startyearChoices <-  c(2041) #2011, 2041, 2051, 2081) # c(2091) # c(2006) #, 2041, 2051, 2081)
 startyearChoices_historical <- c(1991)
 scenarioChoicesEnsemble <- c("historical", sspChoices)
-northernHemExtent <- c( -180, 180, 0, 90)
-southernHemExtent <-c( -180, 180, -90, 0)
-hemisphere <- c("NH", "SH")
 
 yearRange <- 19
-yearRangeSH <- 18 # one less year because of 6 month offset
-
 woptList <- list(gdal=c("COMPRESS=LZW"))
 
 #test values
@@ -55,11 +51,15 @@ readRast_thi_cattle <- function(yearNumber) {
   r <- rast(fileNameIn)
   r <- round(r, 2)
 }
-readRast_thi_cattleEnsemble <- function(i) {
-  fileNameIn <- paste0("data/cmip6/THI/thi.cattle_", i, "_", k,  "_", yearSpan, ".tif") # note yearSpan here
+readRast_thi_Ensemble <- function(i) {
+  fileNameIn <- paste0("data/cmip6/THI/thi.", m, "_", i, "_", k,  "_", yearSpan, ".tif") # note yearSpan here
   print(fileNameIn)
   r <- rast(fileNameIn)
-  #  r <- round(rast(fileNameIn), 2)
+  startDate <- paste0(l, "-01-01"); endDate <- paste0(l + yearRange, "-12-31")
+  indices <- seq(as.Date(startDate), as.Date(endDate), 1)
+  indices <- paste0("X", as.character(indices))
+  names(r) <- indices
+  r
 }
 
 readRast_thi_sheep <- function(yearNumber) {
@@ -90,9 +90,8 @@ THIfun_cattle <- function(rh, tmax) {
   thi[tmax < 20] <- 0
   thi <- round(thi, 2)
 }
+
 THIfun_sheep <- function(rh, tmax) {
-  if (is.nan(cellVector[1])) { return(rep(NA, vlend)) }
-  #  print(paste("length tmax: ", (length(tmax))))
   thi <- tmax - ((0.31 - (0.31 * (rh / 100))) * (tmax - 14.4)) 
   thi[tmax < 20] <- 0
   thi <- round(thi, 2)
@@ -117,12 +116,11 @@ THIfun_chicken <- function(tmax, tmin) {
 }
 
 #THI scenarios -----
-speciesChoice <- c("cattle", "goat", "swine", "chicken", "sheep") # cattle
-# speciesChoice <- "goat"
+speciesChoice <- "chicken"
 for (k in sspChoices) {
   for (l in startyearChoices) {
     for (i in modelChoices) {
-     yearSpan <- paste0(l, "_", l + yearRange)
+      yearSpan <- paste0(l, "_", l + yearRange)
       modelName.lower <- tolower(i)
       fileName_tasmax <- paste0(locOfFiles,  modelName.lower, "_", k, "_tasmax_global_daily_", yearSpan, ".tif")
       tmax <- rast(fileName_tasmax)
@@ -130,12 +128,8 @@ for (k in sspChoices) {
       rh <- rast(fileName_rh)
       comb <- sds(rh, tmax)
       
-      # combined <- c(rh, tmax)
-      # nl <- nlyr(combined)
-      # vlend <- nl/2
-      # v2start <- 1 + vlend
-     gc()
-     for (m in speciesChoice) {
+      gc()
+      for (m in speciesChoice) {
         
         fileName_out <- paste0("data/cmip6/THI/thi.", m, "_",  i, "_", k, "_", yearSpan, ".tif")
         print(paste0("fileName out: ", fileName_out))
@@ -151,7 +145,8 @@ for (k in sspChoices) {
         } else {
           print(system.time(r_out <- lapp(comb, fun = funName, filename = fileName_out, overwrite =TRUE, wopt = woptList)))
           print(paste0("app done"))
- #print(system.time(writeRaster(r_out, filename = fileName_out, overwrite=TRUE, wopt = woptList))); flush.console()
+          r_out <- NULL
+          #print(system.time(writeRaster(r_out, filename = fileName_out, overwrite=TRUE, wopt = woptList))); flush.console()
         }
       }
     }
@@ -162,28 +157,31 @@ for (k in sspChoices) {
 k <- "historical"
 for (i in modelChoices) {
   for (l in startyearChoices_historical) {
-    comb <- sds(rh, tmax)
     yearSpan <- paste0(l, "_", l + yearRange)
     modelName.lower <- tolower(i)
     fileName_tasmax <- paste0(locOfFiles,  modelName.lower, "_", k, "_tasmax_global_daily_", yearSpan, ".tif")
     tmax <- rast(fileName_tasmax)
     fileName_rh <- paste0(locOfFiles, modelName.lower, "_", k, "_hurs_global_daily_", yearSpan, ".tif")
     rh <- rast(fileName_rh)
-    for (m in speciesChoice) {
-      fileName_out <- paste0("data/cmip6/THI/thi.", m, "_",  i, "_", k, "_", yearSpan, ".tif")
+    comb <- sds(rh, tmax)
+    
+#    for (m in speciesChoice) {
+      for (m in "chicken") {
+        fileName_out <- paste0("data/cmip6/THI/thi.", m, "_",  i, "_", k, "_", yearSpan, ".tif")
       print(paste0("fileName out: ", fileName_out))
       funName <- paste0("THIfun_", m)
       if (m %in% "chicken") {
         fileName_tasmin <- paste0(locOfFiles,  modelName.lower, "_", k, "_tasmin_global_daily_", yearSpan, ".tif")
         tmin <- rast(fileName_tasmin)
-        
         comb_chicken <- sds(rh, tmin)
         print(system.time(r_out <- lapp(comb_chicken, THIfun_chicken, filename = fileName_out, overwrite =TRUE, wopt = woptList)))
         tmin <- NULL
       } else {
         print(system.time(r_out <- lapp(comb, fun = funName, filename = fileName_out, overwrite =TRUE, wopt = woptList)))
         print(paste0("app done"))
- #       print(system.time(writeRaster(r_out, filename = fileName_out, overwrite=TRUE, wopt = woptList))); flush.console()
+        r_out <- NULL
+        
+        #       print(system.time(writeRaster(r_out, filename = fileName_out, overwrite=TRUE, wopt = woptList))); flush.console()
       }
     }
   }
@@ -209,38 +207,54 @@ for (i in modelChoices) {
 #   }
 # }
 
-# 20 year THI results, historical  ------
-k <- "historical"
-for (i in modelChoices) {
-  for (l in startyearChoices_historical) {
-    yearSpan <- paste0(l, "_", l + yearRange)
-    yearnumberRange <- seq(l, (l + yearRange), 1)
-    gc()
-    x <- lapply(yearnumberRange, readRast_thi_cattle)
-    r <- rast(x)
-    fileNameOut <- paste0("data/cmip6/THI/thi.cattle_", i, "_", k,  "_", yearSpan, ".tif")
-    print(paste0("fileNameOut: ", fileNameOut))
-    print(system.time(writeRaster(r, fileNameOut, overwrite=TRUE, wopt = woptList))); flush.console()
-    r_start <- r_end <- NULL
-    gc()
-  }
-}
-}
+# # 20 year THI results, historical  ------
+# k <- "historical"
+# for (i in modelChoices) {
+#   for (l in startyearChoices_historical) {
+#     yearSpan <- paste0(l, "_", l + yearRange)
+#     gc()
+#     x <- lapply(yearnumberRange, readRast_thi_cattle)
+#     r <- rast(x)
+#     fileNameOut <- paste0("data/cmip6/THI/thi.cattle_", i, "_", k,  "_", yearSpan, ".tif")
+#     print(paste0("fileNameOut: ", fileNameOut))
+#     print(system.time(writeRaster(r, fileNameOut, overwrite=TRUE, wopt = woptList))); flush.console()
+#     r_start <- r_end <- NULL
+#     gc()
+#   }
+# }
 
 # ensemble means -----
 # combine all the spatrasters by model for the hemisphere, time period, start or end of season and scenario and then take the mean across that combo
 
 for (k in scenarioChoicesEnsemble) {
-  for (l in startyearChoices) {
+  for (startYear in startyearChoices) {
+    l <- startYear
     yearSpan <- paste0(l, "_", l + yearRange)
     yearnumberRange <- seq(l, (l + yearRange), 1)
-    if (k %in% "historical") yearSpan <- "1991_2010"
+    if (k %in% "historical") {yearSpan <- "1991_2010"; l <- 1991}
     gc()
-    x <- lapply(modelChoices, readRast_thi_cattleEnsemble)
-    r <- rast(x)
-    fileName_out <- paste0("data/cmip6/THI/ensemble_thi.cattle_", k, "_", yearSpan, ".tif")
-    system.time(r_mean <- app(r, fun = "mean", filename = fileName_out, overwrite = TRUE, woptList = woptList))
-    plot(r_mean, 1, main = paste0("Ensemble mean, THI cattle, ", k, ", period ", yearSpan))
+    startDate <- paste0(l, "-01-01"); endDate <- paste0(l + yearRange, "-12-31")
+    indices <- seq(as.Date(startDate), as.Date(endDate), 1)
+    indices <- paste0("X", as.character(indices))
+    indices_day <- format(as.Date(indices, format = "X%Y-%m-%d"), format = "%j") # %j is day of the year
+    indices_day <- as.numeric(indices_day)
+ #   for (m in speciesChoice) {
+      for (m in "chicken") {
+        print(paste0("species Choice m: ", m))
+ #     funName <- paste0("readRast_thi_", m, "Ensemble")
+      
+      x <- lapply(modelChoices, readRast_thi_Ensemble)
+      r <- rast(x)
+#      fileName_out <- paste0("data/cmip6/THI/ensemble_thi.cattle_", k, "_", yearSpan, ".tif")
+      fileName_out <- paste0("data/cmip6/THI/ensemble_thi.", m, "_", k, "_", yearSpan, ".tif")
+      print(paste0("fileName out: ", fileName_out))
+      # r.mean <- tapp(r, indices_day, fun = mean, na.rm = TRUE)
+ #     x.cv <- tapp(x, indices_day, fun = cv, na.rm = TRUE)
+      
+      system.time(r.mean <- tapp(r, indices_day, fun = mean, na.rm = TRUE, filename = fileName_out, overwrite = TRUE, woptList = woptList))
+      title <- paste0("Ensemble mean, THI ", m, "," , k, ", period ", yearSpan, ", Jan 1.")
+      plot(r.mean, 1, main = paste0("Ensemble mean, THI cattle, ", k, ", period ", yearSpan))
+    }
   }
 }
 
