f7fc4b86e9f6ea89f5696b0f7b56ac70d3b4cd8c
GeraldCNelson <nelson.gerald.c@gmail.com>
2021-02-02 16:16:22 -0700

    eliminated need for globallyUsed.

--- R/growDegreeDaysCalcsNew.R
+++ R/growDegreeDaysCalcsNew.R
@@ -1,7 +1,30 @@
 #  Calculate the number of growing degrees per day for specific crops in specific areas. Main function is globallyUsed.R
-source("R/globallyUsed.R")
+#source("R/globallyUsed.R")
 library(terra)
 library(data.table)
+get_os <- function() {
+  sysinf <- Sys.info()
+  if (!is.null(sysinf)) {
+    os <- sysinf['sysname']
+    if (os == 'Darwin')
+      os <- "osx"
+  } else {## mystery machine
+    os <- .Platform$OS.type
+    if (grepl("^darwin", R.version$os))
+      os <- "osx"
+    if (grepl("linux-gnu", R.version$os))
+      os <- "linux"
+  }
+  tolower(os)
+}
+if (get_os() %in% "osx") {
+  terraOptions(memfrac = 2,  progress = 10, tempdir =  "data/ISIMIP", verbose = TRUE) # need to use a relative path
+  gddsfileOutLoc <- "data/cmip6/growingDegreeDays/"
+  
+}else{
+  terraOptions(progress = 10, tempdir =  "data/ISIMIP", verbose = TRUE) # need to use a relative path
+  gddsfileOutLoc <- "data/tas/"
+}
 woptList <- list(gdal=c("COMPRESS=LZW"))
 woptList <- list(gdal=c("COMPRESS=DEFLATE", "PREDICTOR=3", "ZLEVEL = 6"))
 
@@ -12,14 +35,13 @@ modelChoices <- c( "GFDL-ESM4", "MPI-ESM1-2-HR", "MRI-ESM2-0", "UKESM1-0-LL", "I
 #modelChoices <- c("MPI-ESM1-2-HR", "MRI-ESM2-0", "IPSL-CM6A-LR") #, "MPI-ESM1-2-HR", "MRI-ESM2-0", "IPSL-CM6A-LR") #
 startyearChoices <-  c( 2041, 2081) #2011, 2041, 2051, 2081) # c(2091) # c(2006) #, 2041, 2051, 2081)
 #startyearChoices <-  c(2081) #2011, 2041, 2051, 2081) 
+ann_crop_temp_table <- as.data.table(read_excel("data-raw/crops/ann_crop_temp_table_summary_22082020.xlsx", range = "A1:T26"))
+data.table::setnames(ann_crop_temp_table, old = names(ann_crop_temp_table), new = make.names(names(ann_crop_temp_table)))
+cropChoice_cereals <- ann_crop_temp_table[ICC.crop.classification %in% "Cereal", crop]
 
 yearRange <- 19
-gddsfileOutLoc <- "data/cmip6/growingDegreeDays/"
 
-# commented out, now in the globallyUsed.R script
 library(readxl)
-# ann_crop_temp_table read in in globallyUsed.R
-cropChoice_cereals <- ann_crop_temp_table[ICC.crop.classification %in% "Cereal", crop] # other choices defined in globallyUsed.R
 
 #cropChoices <- c("cropChoice_cereals")
 cropChoices <- cropChoice_cereals
@@ -38,6 +60,9 @@ f_gdd = function(cellVector, tbase, tbase_max){
 
 f_ycalc <- function(cellVector){max(0, min(cellVector, tbase_max)-tbase)}
 
+gddFilesCompleted <- list.files(gddsfileOutLoc, full.names = TRUE)
+gddFilesCompleted <- gddFilesCompleted[!grepl("aux.xml", gddFilesCompleted, fixed = TRUE)]
+
 for (k in sspChoices)  {
   for (i in modelChoices)  {
     for (l in startyearChoices) {
@@ -46,15 +71,13 @@ for (k in sspChoices)  {
       modelName.lower <- tolower(i)
       yearSpan <- paste0(l, "_", l + yearRange)
       
-      gddFilesCompleted <- list.files(gddsfileOutLoc)
-      gddFilesCompleted <- gddFilesCompleted[!grepl("aux.xml", gddFilesCompleted, fixed = TRUE)]
       
       fileIn.tas <- paste0(locOfFiles, modelName.lower, "_", k, "_tas_global_daily_", yearSpan, ".tif")
       tas <- rast(fileIn.tas)
       print("mem_info for tas")
       terra:::.mem_info(tas, 1) 
       
-      system.time(tas <- tas * 1 )
+      if (get_os() %in% "osx") system.time(tas <- tas * 1 )
       
       for (m in cropChoices) {
         print(paste0("crop: ", m))
