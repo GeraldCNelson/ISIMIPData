787e48358d2828829c40b324bc53344aa8b294d9
GeraldCNelson <nelson.gerald.c@gmail.com>
2021-02-03 10:10:38 -0700

    trying new approach to gdd function.

--- R/growDegreeDaysCalcsNew.R
+++ R/growDegreeDaysCalcsNew.R
@@ -1,66 +1,103 @@
 #  Calculate the number of growing degrees per day for specific crops in specific areas. Main function is globallyUsed.R
 #source("R/globallyUsed.R")
-library(terra)
-library(data.table)
-library(readxl)
-get_os <- function() {
-  sysinf <- Sys.info()
-  if (!is.null(sysinf)) {
-    os <- sysinf['sysname']
-    if (os == 'Darwin')
-      os <- "osx"
-  } else {## mystery machine
-    os <- .Platform$OS.type
-    if (grepl("^darwin", R.version$os))
-      os <- "osx"
-    if (grepl("linux-gnu", R.version$os))
-      os <- "linux"
+{library(terra)
+  library(data.table)
+  library(readxl)
+  get_os <- function() {
+    sysinf <- Sys.info()
+    if (!is.null(sysinf)) {
+      os <- sysinf['sysname']
+      if (os == 'Darwin')
+        os <- "osx"
+    } else {## mystery machine
+      os <- .Platform$OS.type
+      if (grepl("^darwin", R.version$os))
+        os <- "osx"
+      if (grepl("linux-gnu", R.version$os))
+        os <- "linux"
+    }
+    tolower(os)
   }
-  tolower(os)
-}
-if (get_os() %in% "osx") {
-  terraOptions(memfrac = 2,  progress = 10, tempdir =  "data/ISIMIP", verbose = TRUE) # need to use a relative path
-  locOfFiles <- "/Volumes/ExtremeSSD3/bigFiles/"
-  gddsfileOutLoc <- "data/cmip6/growingDegreeDays/"
+  if (get_os() %in% "osx") {
+    terraOptions(memfrac = 2,  progress = 10, tempdir =  "data/ISIMIP", verbose = TRUE) # need to use a relative path
+    locOfFiles <- "/Volumes/ExtremeSSD3/bigFiles/"
+    gddsfileOutLoc <- "data/cmip6/growingDegreeDays/"
+    
+  }else{
+    terraOptions(progress = 10, tempdir =  "data/ISIMIP", verbose = TRUE) # need to use a relative path
+    locOfFiles <- "data/tas/"
+    gddsfileOutLoc <- "data/cmip6/growingDegreeDays/"
+  }
+  woptList <- list(gdal=c("COMPRESS=LZW"))
+  woptList <- list(gdal=c("COMPRESS=DEFLATE", "PREDICTOR=3", "ZLEVEL = 6"))
   
-}else{
-  terraOptions(progress = 10, tempdir =  "data/ISIMIP", verbose = TRUE) # need to use a relative path
-  locOfFiles <- "data/tas/"
-  gddsfileOutLoc <- "data/cmip6/growingDegreeDays/"
+  library(Rcpp)
+  
+  cppFunction("
+  NumericVector min_vec(NumericVector vec1, NumericVector vec2) {
+    int n = vec1.size();
+    if(n != vec2.size()) return 0;
+    else {
+      NumericVector out(n);
+      for(int i = 0; i < n; i++) {
+        out[i] = std::min(vec1[i], vec2[i]);
+      }
+      return out;
+    }
+  }
+")
+  
+  cppFunction("
+  NumericVector max_vec(NumericVector vec1, NumericVector vec2) {
+    int n = vec1.size();
+    if(n != vec2.size()) return 0;
+    else {
+      NumericVector out(n);
+      for(int i = 0; i < n; i++) {
+        out[i] = std::max(vec1[i], vec2[i]);
+      }
+      return out;
+    }
+  }
+")
+  
+  sspChoices <- c("ssp126", "ssp585") 
+  #sspChoices <- c("ssp585") 
+  modelChoices <- c( "GFDL-ESM4", "MPI-ESM1-2-HR", "MRI-ESM2-0", "UKESM1-0-LL", "IPSL-CM6A-LR") 
+  #modelChoices <- c("MPI-ESM1-2-HR", "MRI-ESM2-0", "IPSL-CM6A-LR") #, "MPI-ESM1-2-HR", "MRI-ESM2-0", "IPSL-CM6A-LR") #
+  startyearChoices <-  c( 2041, 2081) #2011, 2041, 2051, 2081) # c(2091) # c(2006) #, 2041, 2051, 2081)
+  #startyearChoices <-  c(2081) #2011, 2041, 2051, 2081) 
+  ann_crop_temp_table <- as.data.table(read_excel("data-raw/crops/ann_crop_temp_table_summary_22082020.xlsx", range = "A1:T26"))
+  data.table::setnames(ann_crop_temp_table, old = names(ann_crop_temp_table), new = make.names(names(ann_crop_temp_table)))
+  cropChoice_cereals <- ann_crop_temp_table[ICC.crop.classification %in% "Cereal", crop]
+  
+  yearRange <- 19
+  
+  cropChoices <- cropChoice_cereals
+  #test values
+  i <- "UKESM1-0-LL"
+  k <- "ssp585"
+  l <- 2041
+  m <- "Wheat"
+  
+  # f_gdd = function(cellVector, tbase, tbase_max){
+  #   if (is.nan(cellVector[1])) {return(cellVector)}
+  # #  y <- clamp(cellVector, lower = tbase, upper = tbase_max)
+  #   y <- sapply(cellVector, FUN = f_ycalc) # from Toshi email May 3, 2020
+  #   return(y)
+  # }
+  # 
+  # f_ycalc <- function(cellVector){max(0, min(cellVector, tbase_max)-tbase)}
+  
+  f_gdd = function(cellVector, tbase, tbase_max){
+    if (is.nan(cellVector[1])) {return(cellVector)}
+    y <- pmax(0, pmin(cellVector, tbase_max)-tbase)
+    return(y)
+  }
+  gddFilesCompleted <- list.files(gddsfileOutLoc,  full.names = TRUE)
+  gddFilesCompleted <- gddFilesCompleted[!grepl("aux.xml", gddFilesCompleted, fixed = TRUE)]
+  gddFilesCompleted <- gsub("//", "/", gddFilesCompleted)
 }
-woptList <- list(gdal=c("COMPRESS=LZW"))
-woptList <- list(gdal=c("COMPRESS=DEFLATE", "PREDICTOR=3", "ZLEVEL = 6"))
-
-sspChoices <- c("ssp126", "ssp585") 
-#sspChoices <- c("ssp585") 
-modelChoices <- c( "GFDL-ESM4", "MPI-ESM1-2-HR", "MRI-ESM2-0", "UKESM1-0-LL", "IPSL-CM6A-LR") 
-#modelChoices <- c("MPI-ESM1-2-HR", "MRI-ESM2-0", "IPSL-CM6A-LR") #, "MPI-ESM1-2-HR", "MRI-ESM2-0", "IPSL-CM6A-LR") #
-startyearChoices <-  c( 2041, 2081) #2011, 2041, 2051, 2081) # c(2091) # c(2006) #, 2041, 2051, 2081)
-#startyearChoices <-  c(2081) #2011, 2041, 2051, 2081) 
-ann_crop_temp_table <- as.data.table(read_excel("data-raw/crops/ann_crop_temp_table_summary_22082020.xlsx", range = "A1:T26"))
-data.table::setnames(ann_crop_temp_table, old = names(ann_crop_temp_table), new = make.names(names(ann_crop_temp_table)))
-cropChoice_cereals <- ann_crop_temp_table[ICC.crop.classification %in% "Cereal", crop]
-
-yearRange <- 19
-
-cropChoices <- cropChoice_cereals
-#test values
-i <- "UKESM1-0-LL"
-k <- "ssp585"
-l <- 2041
-m <- "Wheat"
-
-f_gdd = function(cellVector, tbase, tbase_max){
-  if (is.nan(cellVector[1])) {return(cellVector)}
-#  y <- clamp(cellVector, lower = tbase, upper = tbase_max)
-  y <- sapply(cellVector, FUN = f_ycalc) # from Toshi email May 3, 2020
-  return(y)
-}
-
-f_ycalc <- function(cellVector){max(0, min(cellVector, tbase_max)-tbase)}
-
-gddFilesCompleted <- list.files(paste0(getwd(), gddsfileOutLoc),  full.names = TRUE)
-gddFilesCompleted <- gddFilesCompleted[!grepl("aux.xml", gddFilesCompleted, fixed = TRUE)]
 
 for (k in sspChoices)  {
   for (i in modelChoices)  {
@@ -70,13 +107,12 @@ for (k in sspChoices)  {
       modelName.lower <- tolower(i)
       yearSpan <- paste0(l, "_", l + yearRange)
       
-      
       fileIn.tas <- paste0(locOfFiles, modelName.lower, "_", k, "_tas_global_daily_", yearSpan, ".tif")
       tas <- rast(fileIn.tas)
       print("mem_info for tas")
       terra:::.mem_info(tas, 1) 
       
-      if (get_os() %in% "osx") system.time(tas <- tas * 1 )
+      if (get_os() %in% "osx") print(system.time(tas <- tas * 1))
       
       for (m in cropChoices) {
         print(paste0("crop: ", m))
@@ -86,7 +122,7 @@ for (k in sspChoices)  {
           tbase <- ann_crop_temp_table[crop == m, Tbase]
           tbase_max <- ann_crop_temp_table[crop == m, Tbase_max]
           print(Sys.time())
-         print(system.time(gdd <- app(tas, fun=f_gdd, tbase, tbase_max, filename = fileName_out, overwrite=TRUE, wopt = woptList)))
+          print(system.time(gdd <- app(tas, fun=f_gdd, tbase, tbase_max, filename = fileName_out, overwrite=TRUE, wopt = woptList)))
           print(Sys.time())
           print(paste0("gdd file out name: ", fileName_out))
           #         writeRaster(round(gdd, 1), filename = paste0(gddsfileOutLoc, fileName_out, ".tif"), format = "GTiff", overwrite = TRUE, wopt = woptList)
