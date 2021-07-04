89adbaf387c5a986c1d7f41ff970c0cedb24f77e
GeraldCNelson <nelson.gerald.c@gmail.com>
2021-07-04 14:40:58 -0600

    added more of the functions

--- R/rewriteTGWBL.R
+++ R/rewriteTGWBL.R
@@ -1,7 +1,7 @@
 
 
 
-wbgt.Liljegren <- function (tas, dewp, relh, wind, radiation, dates, lon, lat, tolerance = 1e-04, noNAs = TRUE, swap = FALSE, hour = FALSE) {
+wbgt.Liljegren <- function(tas, dewp, relh, wind, radiation, dates, lon, lat, tolerance = 1e-04, noNAs = TRUE, swap = FALSE, hour = FALSE) {
   propDirect <- 0.8
   Pair <- 1010
   MinWindSpeed <- 0.1
@@ -109,4 +109,142 @@ fTnwb <- function (tas, dewp, relh, Pair, wind, min.speed, radiation, propDirect
   opt <- stats::optimize(fr, range(Tdew - 1, Tair + 1), Tair, Pair, tol = tolerance)
   Tnwb <- opt$minimum - 273.15
   return(Tnwb)
+}
+
+degToRad <- function(angleDeg){
+  degToRad <- pi * angleDeg / 180
+  return(degToRad)
+}
+
+calZenith <- function (dates, lon, lat, hour = FALSE) {
+  EQTIME1 <- 229.18
+  EQTIME2 <- 7.5e-05
+  EQTIME3 <- 0.001868
+  EQTIME4 <- 0.032077
+  EQTIME5 <- 0.014615
+  EQTIME6 <- 0.040849
+  DECL1 <- 0.006918
+  DECL2 <- 0.399912
+  DECL3 <- 0.070257
+  DECL4 <- 0.006758
+  DECL5 <- 0.000907
+  DECL6 <- 0.002697
+  DECL7 <- 0.00148
+  if (hour) {
+    d0 <- strftime(dates, format = "%Y-%m-%d %H:%M:%S", usetz = TRUE, tz = "UTC")
+    d1 <- strptime(d0, format = "%Y-%m-%d %H:%M:%S", tz = "UTC")
+    utc.hour <- as.numeric(format(d1, "%H"))
+  }
+  else {
+    d1 <- strptime(dates, format = "%Y-%m-%d")
+    utc.hour <- 12
+  }
+  year <- as.numeric(format(d1, "%Y"))
+  doy <- as.numeric(strftime(d1, format = "%j"))
+  if (is.leapyear(year)) 
+    dpy = 366
+  else dpy = 365
+  RadLon <- degToRad(lon)
+  RadLat <- degToRad(lat)
+  Gamma <- 2 * pi * ((doy - 1) + (utc.hour/24))/dpy
+  EquTime <- EQTIME1 * (EQTIME2 + EQTIME3 * cos(Gamma) - EQTIME4 * 
+                          sin(Gamma) - EQTIME5 * cos(2 * Gamma) - EQTIME6 * sin(2 * Gamma))
+  Decli <- DECL1 - DECL2 * cos(Gamma) + DECL3 * sin(Gamma) - 
+    DECL4 * cos(2 * Gamma) + DECL5 * sin(2 * Gamma) - DECL6 * cos(3 * Gamma) + DECL7 * sin(3 * Gamma)
+  TimeOffset <- 0
+  TrueSolarTime <- (utc.hour * 60) + TimeOffset
+  HaDeg <- ((TrueSolarTime/4) - 180)
+  HaRad <- degToRad(HaDeg)
+  CosZen <- (sin(RadLat) * sin(Decli) + cos(RadLat) * cos(Decli) * cos(HaRad))
+  if (CosZen > 1) CosZen <- 1
+  if (CosZen < -1) CosZen <- -1
+  SZARad <- acos(CosZen)
+  SZA <- radToDeg(SZARad)
+  return(SZA)
+}
+
+is.leapyear <- function(year){
+  return(((year %% 4 == 0) & (year %% 100 != 0)) | (year %% 400 == 0))
+}
+
+h_sphere_in_air <- function(Tk, Pair, speed, min.speed, diam.globe) {
+  # Constants
+  m.air <- 28.97
+  r.gas <- 8314.34
+  r.air <- r.gas / m.air
+  cp <- 1003.5 # heat capacity at constant pressure of dry air
+  Pr <- cp / (cp + 1.25 * r.air)
+  
+  # Calculate the thermal conductivity of air, W/(m K)
+  therm.con <- thermal_cond(Tk)
+  
+  # Density of the air
+  density <- Pair * 100 / (r.air * Tk)
+  if(speed < min.speed) speed <- min.speed
+  
+  # Reynolds number
+  Re <- speed * density * diam.globe / viscosity(Tk)
+  
+  # Nusselt number
+  Nu <- 2 + 0.6 * Re ^ 0.5 * Pr ^ 0.3333
+  
+  # Convective heat tranfer coefficient for flow around a sphere, W/(m2 K)
+  h_sphere_in_air <- Nu * therm.con / diam.globe 
+  
+  return(h_sphere_in_air)
+}
+
+viscosity <- function(Tk){
+  omega <- (Tk / 97 - 2.9) / 0.4 * (-0.034) + 1.048
+  viscosity <- 0.0000026693 * (28.97 * Tk) ^ 0.5 / (3.617 ^ 2 * omega)
+  return(viscosity)
+}
+
+h_evap <- function(Tk) {
+evap <- (313.15 - Tk) / 30 * (-71100) + 2407300
+return(evap)
+}
+
+esat <- function(Tk) {
+  esat <- 6.1121 * exp(17.502 * (Tk - 273.15) / (Tk - 32.18))
+  esat <- 1.004 * esat  #correction for moist air, if pressure is not available; for pressure > 800 mb
+  return(esat)
+}
+
+h_cylinder_in_air <- function(Tk, Pair, speed, min.speed, diam.wick) {
+  # Constants
+  m.air <- 28.97
+  r.gas <- 8314.34
+  r.air <- r.gas / m.air
+  cp <- 1003.5 # heat capaticy at constant pressure of dry air
+  Pr <- cp / (cp + (1.25 * r.air))
+  
+  # Calculate the thermal conductivity of air, W/(m K)
+  therm.con <- thermal_cond(Tk)
+  
+  # Density of the air
+  density <- Pair * 100 / (r.air * Tk)
+  if(speed < min.speed) speed <- min.speed
+  
+  # Reynolds number
+  Re <- speed * density * diam.wick / viscosity(Tk)
+  
+  # Nusselt number
+  Nu <- 0.281 * Re ^ 0.6 * Pr ^ 0.44
+  
+  # Convective heat transfer coefficient in W/(m2 K) for a long cylinder in cross flow
+  h_cylinder_in_air <- Nu * therm.con / diam.wick  
+  return(h_cylinder_in_air)
+}
+
+thermal_cond <- function(Tk) {
+  # Constants
+  m.air <- 28.97
+  r.gas <- 8314.34
+  r.air <- r.gas / m.air
+  cp <- 1003.5 # heat capacity at constant pressure of dry air
+  
+  # Calculate the thermal conductivity of air, W/(m K)
+  therm.con <- (cp + 1.25 * r.air) * viscosity(Tk)
+  return(therm.con)
 }
